12670256e51301da186ba5fa68e5b60c5d22e1b3
HARSHVARDHAN PUROHIT <harshvardhanpurohit2020@gmail.com>
2022-08-14 03:18:54 +0530

    final

--- src/Components/CampusTourCards.js
+++ src/Components/CampusTourCards.js
@@ -5,7 +5,7 @@ const CampusTourCards = (props) => {
   return (
     <>
     <div class="rounded overflow-hidden shadow-lg shadow-green-400 border-solid border-white mx-auto lg:h-[25rem] lg:w-[44rem]">
-    <ReactPlayer muted loop playing height="100%" width="100%" url={'https://www.youtube.com/watch?v=a0VQ68ND5vk&t=204s'}></ReactPlayer>
+    <ReactPlayer muted loop playing height="100%" width="100%" controls url={'https://www.youtube.com/watch?v=a0VQ68ND5vk&t=204s'}></ReactPlayer>
     </div>
     </>
   )
