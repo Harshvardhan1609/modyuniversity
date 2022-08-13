6b4e6895407bb215df9648d7d7467cc759626689
HARSHVARDHAN PUROHIT <harshvardhanpurohit2020@gmail.com>
2022-08-14 03:44:19 +0530

    intial

--- src/Components/CampusTourCards.js
+++ src/Components/CampusTourCards.js
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player'
 const CampusTourCards = (props) => {
   return (
     <div id='campustour'>
-    <div class="rounded-2xl overflow-hidden shadow-lg shadow-green-400 border-solid border-white mx-auto h-[14rem] w-[19rem] lg:h-[25rem] lg:w-[44rem]"> 
+    <div class="rounded-2xl overflow-hidden shadow-lg shadow-green-400 border-solid hover:shadow-2xl hover:shadow-emerald-600 duration-500 border-white mx-auto h-[14rem] w-[19rem] lg:h-[25rem] lg:w-[44rem]"> 
     <ReactPlayer muted loop playing height="100%" width="100%" controls url={'https://www.youtube.com/watch?v=a0VQ68ND5vk&t=204s'}></ReactPlayer>
     </div>
     </div>
