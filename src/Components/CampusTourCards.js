986635d29ee0f137772ac6ff3d1178299b1ceffe
HARSHVARDHAN PUROHIT <harshvardhanpurohit2020@gmail.com>
2022-08-14 03:09:17 +0530

    intial2

--- src/Components/CampusTourCards.js
+++ src/Components/CampusTourCards.js
@@ -4,9 +4,9 @@ import ReactPlayer from 'react-player'
 const CampusTourCards = (props) => {
   return (
     <>
-    <ReactPlayer muted loop playing height="200%" width="100%" url={'https://www.youtube.com/watch?v=a0VQ68ND5vk&t=204s'}></ReactPlayer>
-    <br />
-    <br />
+    <div class="rounded overflow-hidden shadow-lg shadow-green-400 border-solid border-white mx-auto sm:h-[13rem] sm:w-[22rem] h-[16rem] w-[26rem] md:h-[25rem] md:w-[44rem]">
+    <ReactPlayer muted loop playing height="100%" width="100%" url={'https://www.youtube.com/watch?v=a0VQ68ND5vk&t=204s'}></ReactPlayer>
+    </div>
     </>
   )
 }
